7f79a3c72f8aa0745e7bedb874a86b1243e1a5c4
Gabriela Zambrano <g.zambranoz@uniandes.edu.co>
2025-04-29 08:26:12 -0500

    G

--- monitorear.py
+++ monitorear.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route('/monitoreo/errorUsuarios', methods=['GET'])
 def mandar_mensaje_advertencia():
-    print("Se entro a la funcion!")
-    """account_sid = 'AC55994d626e2eb71e524c938bf8c27b07'  
+    """print("Se entro a la funcion!")
+    account_sid = 'AC55994d626e2eb71e524c938bf8c27b07'  
     auth_token = '78154485797bd9dc9a5ee57f5ab97914' 
 
     
@@ -21,6 +21,7 @@ def mandar_mensaje_advertencia():
         to=to_number
     )
     print("mensaje enviado")"""
+    return "Mensaje enviado correctamente", 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
